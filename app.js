2a069f74dd7b542f361de7ec04279d7e91f6696e
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-09-08 15:02:06 +0530

    Added logic to save transactions

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import apiCall from "./utils/apiCall.js";
 import UserRoutes from "./routes/UserRoutes.js";
 import TransactionRoutes from "./routes/transactionRoutes.js";
 import { protect } from "./middlewares/protect.js";
+import Transaction from "./models/TransactionModel.js";
 
 dotenv.config();
 
@@ -34,12 +35,27 @@ app.use(cors());
 app.use("/users", UserRoutes);
 app.use("/transactions", TransactionRoutes);
 
+app.get("/dashboard", protect, (req, res) => {
+  const user = req.user;
+  console.dir(user.id);
+  res.status(200).json({ message: "Welcome to the dashboard!" });
+});
+
 app.post("/api/putdata", protect, async (req, res) => {
   try {
     const apiResponse = await apiCall(req.body);
     const match = apiResponse.match(/```json\s*([\s\S]*?)\s*```/);
     const jsonString = match ? match[1] : apiResponse;
     const parsedData = JSON.parse(jsonString);
+    for(let item of parsedData){
+      let t = new Transaction({
+        userId: req.user.id,
+        amount: item.amount,
+        date: new Date(item.transactionDate),
+        category: item.category
+      });
+      await t.save();
+    }
     res.status(200).json(parsedData);
   } catch (error) {
     console.error("An error occurred:", error.message);
--- models/TransactionModel.js
+++ models/TransactionModel.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const transactionSchema = new mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
     amount: {type: Number, required: true},
+    category: {type: String, required: true},
     createdAt: {type: Date, default: Date.now},
 });
 
